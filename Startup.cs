22f7251b5e100927ae2b9fa8239db3fea5d2002d
Wladimilson <9863709+wladimilson@users.noreply.github.com>
2019-11-28 11:20:26 -0300

    Configuração inicial

---
+++ Startup.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Carter;
+
+namespace CarterAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCarter();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => endpoints.MapCarter());
+        }
+    }
+}
