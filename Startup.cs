d23d6bb85c793788a0561c7831c2b7e5d7293380
Wladimilson <9863709+wladimilson@users.noreply.github.com>
2020-10-15 12:10:25 -0300

    Configuração para a publicação da aplicação

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Carter;
 
 namespace CarterAPI
@@ -20,6 +21,11 @@ namespace CarterAPI
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             app.UseRouting();
 
             app.UseSwaggerUI(opt =>
